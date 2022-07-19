0e3dfb187dfc53965d5a690622262afa01085215
Singh Amit <singhami@ap.dtcs.local>
2022-07-20 00:44:05 +0530

    first commit client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Users } from './models/users.models';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'DatingApp';
+  users:Users[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.getUsers();
+  }
+  getUsers() {
+    this.http.get<Users[]>('https://localhost:5001/api/users')
+    .subscribe({
+      next: (response) => {
+        this.users = response;
+      },
+      error: (error) => console.log(error)
+    });
+  }  
 }
---
+++ src/app/models/users.models.ts
@@ -0,0 +1,4 @@
+export class Users {
+    id:number = 0;
+    username: string = '';
+}
